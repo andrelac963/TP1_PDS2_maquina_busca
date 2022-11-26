247572ada957d6e3f7fe831b091994605aa3f410
andrelac963 <andrelac963@gmail.com>
2022-11-26 11:28:49 -0300

    Dev: menu de opções de pesquisa completo

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,39 @@
 #include <string>
 #include "indexing.hpp"
 
+using namespace std;
+
 int main()
 {
-  string pesquisa;
-  indexing teste;
- 
-  cout << "O que deseja buscar?\n ";
-  cin >> pesquisa;
+  string search, option;
+  indexing index;
 
-  teste.recovery(pesquisa);
+  do
+  {
+    cout << "O que deseja buscar?\n ";
+    cin >> search;
+
+    index.recovery(search);
+
+    do
+    {
+      cout << "\nDeseja fazer outra pesquisa? (s/n)" << endl;
+      cin >> option;
+
+      if (option != "s" && option != "n")
+      {
+        cout << "Opção inválida!" << endl;
+      }
+
+    } while (option != "s" && option != "n");
+
+    if (option == "s")
+    {
+      cout << "\n--------------------------------------------\n"
+           << endl;
+    }
+
+  } while (option == "s");
 
   return 0;
 }
